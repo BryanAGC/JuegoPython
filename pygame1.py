80303f42c77d479f039ab9b7cef6de127c38bf79
Bryan21developer <91568815+BryanAGC@users.noreply.github.com>
2024-10-14 22:00:23 -0600

    Update pygame1.py
    AGREGAR EL PERSONAJE A LA VENTANA PRINCIPAL

--- pygame1.py
+++ pygame1.py
@@ -1,8 +1,12 @@
+import pygame
+from personaje import cubo
+
 ANCHO = 1000
 ALTO = 800
 VENTANA = pygame.display.set_mode([ALTO,ANCHO])
 
 jugando=True
+cubo = Cubo(100,100)
 
 while jugando:
   eventos=pygame.event.get()
@@ -10,6 +14,7 @@ while jugando:
   for evento in eventos:
     if evento.type ==pygame.QUIT:
       jugando = False
-  
-  pygame.display.update()
+      
+    cubo.dibujar(VENTANA)
+    pygame.display.update()
   quit()
