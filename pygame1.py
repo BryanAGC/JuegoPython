07be386456f840ac574f94fc889f89d91224d48b
Bryan21developer <91568815+BryanAGC@users.noreply.github.com>
2024-10-15 21:30:18 -0600

    Update pygame1.py
    GESTIONAR LAS TECLAS DE ENTRADA

--- pygame1.py
+++ pygame1.py
@@ -7,14 +7,26 @@ VENTANA = pygame.display.set_mode([ALTO,ANCHO])
 
 jugando=True
 cubo = Cubo(100,100)
-
+def gestionar_teclasF(teclas):
+  if teclas[pygame.K_w]:
+    cubo.y -= cubo.velocidad
+  if teclas[pygame.K_s]:
+    cubo.y += cubo.velocidad
+  if teclas[pygame.K_a]:
+    cubo.x -= cubo.velocidad
+  if teclas[pygame.K_d]:
+    cubo.x += cubo.velocidad
+    
 while jugando:
   eventos=pygame.event.get()
+  
+teclas = pygame.key.get_pressed()
+gestiona_teclas(teclas)
 
   for evento in eventos:
     if evento.type ==pygame.QUIT:
       jugando = False
-      
+
     cubo.dibujar(VENTANA)
     pygame.display.update()
   quit()
