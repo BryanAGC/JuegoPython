cf90f221fd92ce4909f83e8d6638834d9ad4ceae
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-17 11:34:00 -0600

    Correccion de errores

--- personaje.py
+++ personaje.py
@@ -1,18 +1,15 @@
 import pygame
 
 class Cubo:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
-    self.ancho = 50
-    self.alto  = 50
-    self.velocidad = 1
-    self.color = "red"
-    self.rec = pygame.Rect(self.x,self.ancho, self.alto)
-    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 1
+        self.color = "red"
+        self.rec = pygame.Rect(self.x, self.y, self.ancho, self.alto)
 
-  
-  def  dibujar(self, ventana):
-    pygame.draw.rect(ventana, self.color, self.rect)
-    self.rec = pygame.Rect(self.x,self.ancho, self.alto)
-         
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana, self.color, self.rec)
+        self.rec = pygame.Rect(self.x, self.y, self.ancho, self.alto)
--- pygame1.py
+++ pygame1.py
@@ -1,33 +1,37 @@
 import pygame
-from personaje import cubo
+from personaje import Cubo  # Asegúrate de que 'Cubo' esté bien definido en el módulo 'personaje'
 
 ANCHO = 1000
 ALTO = 800
-VENTANA = pygame.display.set_mode([ALTO,ANCHO])
+VENTANA = pygame.display.set_mode([ANCHO, ALTO])  # Invertí ALTO y ANCHO para que sea correcto
 
-jugando=True
-cubo = Cubo(100,100)
-def gestionar_teclasF(teclas):
-  if teclas[pygame.K_w]:
-    cubo.y -= cubo.velocidad
-  if teclas[pygame.K_s]:
-    cubo.y += cubo.velocidad
-  if teclas[pygame.K_a]:
-    cubo.x -= cubo.velocidad
-  if teclas[pygame.K_d]:
-    cubo.x += cubo.velocidad
-    
+pygame.init()  # Inicializar pygame es importante antes de empezar
+
+jugando = True
+cubo = Cubo(100, 100)
+
+def gestionar_teclas(teclas):
+    if teclas[pygame.K_w]:
+        cubo.y -= cubo.velocidad
+    if teclas[pygame.K_s]:
+        cubo.y += cubo.velocidad
+    if teclas[pygame.K_a]:
+        cubo.x -= cubo.velocidad
+    if teclas[pygame.K_d]:
+        cubo.x += cubo.velocidad
+
 while jugando:
-  eventos=pygame.event.get()
-  
-teclas = pygame.key.get_pressed()
-gestiona_teclas(teclas)
+    eventos = pygame.event.get()
 
-  for evento in eventos:
-    if evento.type ==pygame.QUIT:
-      jugando = False
+    teclas = pygame.key.get_pressed()
+    gestionar_teclas(teclas)
+
+    for evento in eventos:
+        if evento.type == pygame.QUIT:
+            jugando = False
+
     VENTANA.fill("black")
     cubo.dibujar(VENTANA)
     pygame.display.update()
-  quit()
-       
+
+pygame.quit()
